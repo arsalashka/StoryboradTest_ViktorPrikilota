1d61b40b8146d0d4cf91e6f58bcb102b4161153e
Arsalan Balzhinimaev <arsalashka@gmail.com>
2024-04-24 16:28:45 -0500

    add UI elements to loginVC and signupVC

---
+++ StoryboradTest_ViktorPrikilota/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboradTest_ViktorPrikilota
+//
+//  Created by Arsalan on 24.04.2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StoryboradTest_ViktorPrikilota/SingupViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SingupViewController.swift
+//  StoryboradTest_ViktorPrikilota
+//
+//  Created by Arsalan on 24.04.2024.
+//
+
+import UIKit
+
+class SingupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
